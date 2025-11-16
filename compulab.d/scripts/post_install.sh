a74625c9d39fcef318f22e1988b7714715921fe3
Valentin Raevsky <valentin@compulab.co.il>
2025-11-16 23:35:59 +0200

    Update post_install.sh
    Signed-off-by: Valentin Raevsky <valentin@compulab.co.il>

--- compulab.d/scripts/post_install.sh
+++ compulab.d/scripts/post_install.sh
@@ -3,20 +3,24 @@
 VERSION=${1}
 
 update_boot_files() {
-	cp /boot/initrd /boot/initrd-${VERSION}
-	cp /boot/Image /boot/Image-${VERSION}
+    mkdir /boot/compulab/backup -p
+    cp /boot/initrd /boot/compulab/initrd-${VERSION}
+    cp /boot/Image /boot/compulab/Image-${VERSION}
+    cp /boot/initrd /boot/compulab/backup/initrd-${VERSION}
+    cp /boot/Image /boot/compulab/backup/Image-${VERSION}
 }
 
 update_boot_config() {
-	sed -i "s/\(^ .*LINUX.*Image\).*$/\1-${VERSION}/g"          /boot/extlinux/extlinux.conf
-	sed -i "s/\(^ .*INITRD.*initrd\).*$/\1-${VERSION}/g"i       /boot/extlinux/extlinux.conf
-	sed -i 's/\(^. *APPEND ${cbootargs}\).*/\1 net.ifnames=0/g' /boot/extlinux/extlinux.conf
-
+    sed -i "s/\(^ .*LINUX \).*$/\1\/boot\/compulab\/Image-${VERSION}/g"   /boot/extlinux/extlinux.conf
+    sed -i "s/\(^ .*INITRD \).*$/\1\/boot\/compulab\/initrd-${VERSION}/g" /boot/extlinux/extlinux.conf
+    sed -i "s/\(^. *APPEND \${cbootargs}\).*/\1 net.ifnames=0/g"          /boot/extlinux/extlinux.conf
+    # sed -i "/FDT/d" /boot/extlinux/extlinux.conf
+    # sed -i "/initrd-${VERSION}/a\      FDT /boot/dtbs/tegra234-p3768-0000+p3767-0000-nv-super-device.dtb" /boot/extlinux/extlinux.conf
 }
 
 prevent_bootloader_update() {
-	local key_file="/opt/nvidia/l4t-packages/.nv-l4t-disable-boot-fw-update-in-preinstall"
-	mkdir -p $(dirname ${key_file}) && touch ${key_file}
+    local key_file="/opt/nvidia/l4t-packages/.nv-l4t-disable-boot-fw-update-in-preinstall"
+    mkdir -p $(dirname ${key_file}) && touch ${key_file}
 }
 
 nv-update-initrd
