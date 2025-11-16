8cf21bca717b3fd7b784634784ce08d378172c75
Valentin Raevsky <valentin@compulab.co.il>
2025-11-16 16:33:55 +0200

    Update post_install.sh
    Add prevent_bootloader_update()
    
    Signed-off-by: Valentin Raevsky <valentin@compulab.co.il>

--- compulab.d/scripts/post_install.sh
+++ compulab.d/scripts/post_install.sh
@@ -8,10 +8,18 @@ update_boot_files() {
 }
 
 update_boot_config() {
-	sed -i "s/\(^ .*LINUX.*Image\).*$/\1-${VERSION}/g"    /boot/extlinux/extlinux.conf
-	sed -i "s/\(^ .*INITRD.*initrd\).*$/\1-${VERSION}/g"  /boot/extlinux/extlinux.conf
+	sed -i "s/\(^ .*LINUX.*Image\).*$/\1-${VERSION}/g"          /boot/extlinux/extlinux.conf
+	sed -i "s/\(^ .*INITRD.*initrd\).*$/\1-${VERSION}/g"i       /boot/extlinux/extlinux.conf
+	sed -i 's/\(^. *APPEND ${cbootargs}\).*/\1 net.ifnames=0/g' /boot/extlinux/extlinux.conf
+
+}
+
+prevent_bootloader_update() {
+	local key_file="/opt/nvidia/l4t-packages/.nv-l4t-disable-boot-fw-update-in-preinstall"
+	mkdir -p $(dirname ${key_file}) && touch ${key_file}
 }
 
 nv-update-initrd
 update_boot_files
 update_boot_config
+prevent_bootloader_update
