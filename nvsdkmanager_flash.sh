5a39bc7818a16064337e236931a4faff9d115b9c
Valentin Raevsky <valentin@compulab.co.il>
2025-11-21 12:36:36 +0200

    nvsdkmanager_flash: Update configuration name
    Signed-off-by: Valentin Raevsky <valentin@compulab.co.il>

--- nvsdkmanager_flash.sh
+++ nvsdkmanager_flash.sh
@@ -5,7 +5,7 @@ deploy_bootloader_rootfs() {
     --external-device nvme0n1p1 \
     -c tools/kernel_flash/flash_l4t_t234_nvme.xml \
     -p "-c bootloader/generic/cfg/flash_t234_qspi.xml" \
-    --keep --showlogs --network usb0 edge-ai-rev1v2 external
+    --keep --showlogs --network usb0 edge-ai external
 }
 
 deploy_bootloader_rootfs
