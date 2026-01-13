a7f30330b5c75ed67a85707f93a5d902467bfeb0
Valentin Raevsky <valentin@compulab.co.il>
2026-01-13 19:04:24 +0200

    rootfs_update: Issue scripts instead of printing their names
    Signed-off-by: Valentin Raevsky <valentin@compulab.co.il>

--- compulab.d/rootfs/opt/compulab/rootfs_update.sh
+++ compulab.d/rootfs/opt/compulab/rootfs_update.sh
@@ -5,7 +5,7 @@ INITD_FOLDER=/opt/compulab/init.d
 init_main() {
     [[ -d ${INITD_FOLDER} ]] || return 0
     for script in ${INITD_FOLDER}/*; do
-        echo ${script}
+        ${script}
     done
 }
 
