bad921ccf068cfaf280a7af802538399840b2e4d
Igor Derzhavets <igor.derzhavets@compulab.co.il>
2026-06-22 16:02:57 +0300

    flash: select QSPI config by L4T layout

--- compulab.d/overlays/nvsdkmanager_flash.sh
+++ compulab.d/overlays/nvsdkmanager_flash.sh
@@ -1,10 +1,13 @@
 #!/bin/bash -x
 
 deploy_bootloader_rootfs() {
+    local qspi_cfg="bootloader/generic/cfg/flash_t234_qspi.xml"
+    [[ -f bootloader/t186ref/cfg/flash_t234_qspi.xml && ! -f ${qspi_cfg} ]] && qspi_cfg="bootloader/t186ref/cfg/flash_t234_qspi.xml"
+
     ./tools/kernel_flash/l4t_initrd_flash.sh \
     --external-device nvme0n1p1 \
     -c tools/kernel_flash/flash_l4t_t234_nvme.xml \
-    -p "-c bootloader/generic/cfg/flash_t234_qspi.xml" \
+    -p "-c ${qspi_cfg}" \
     --keep --showlogs --network usb0 edge-ai external
 }
 
