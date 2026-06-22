5f530dc6654cb548db878241c068ed4448adac88
Igor Derzhavets <igor.derzhavets@compulab.co.il>
2026-06-22 16:02:57 +0300

    mfg: select QSPI config by L4T layout

--- compulab.d/mfg/tools/mfi_dev_create.sh
+++ compulab.d/mfg/tools/mfi_dev_create.sh
@@ -2,8 +2,10 @@
 
 prepare_mfi() {
     local _board="000${1}"
+    local qspi_cfg="bootloader/generic/cfg/flash_t234_qspi.xml"
     pushd ${L4T_ROOT}
-    sudo BOARDID=3767 BOARDSKU=${_board} FAB=TS4 ./tools/kernel_flash/l4t_initrd_flash.sh --external-device nvme0n1p1 -c tools/kernel_flash/flash_l4t_t234_nvme.xml -p "-c bootloader/generic/cfg/flash_t234_qspi.xml" --no-flash --massflash 1 --showlogs --network usb0 edge-ai external
+    [[ -f bootloader/t186ref/cfg/flash_t234_qspi.xml && ! -f ${qspi_cfg} ]] && qspi_cfg="bootloader/t186ref/cfg/flash_t234_qspi.xml"
+    sudo BOARDID=3767 BOARDSKU=${_board} FAB=TS4 ./tools/kernel_flash/l4t_initrd_flash.sh --external-device nvme0n1p1 -c tools/kernel_flash/flash_l4t_t234_nvme.xml -p "-c ${qspi_cfg}" --no-flash --massflash 1 --showlogs --network usb0 edge-ai external
     mv mfi_edge-ai.tar.gz ${_board}_mfi_edge-ai.tar.gz
     popd
 }
